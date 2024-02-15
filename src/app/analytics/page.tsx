b8fe0eb406229145e1ce410dbc06c5258392cb18
PersianDavinci <persiandavinci@gmail.com>
2024-02-15 15:50:57 +0330

    add components and AnalyticsDashboard.tsx and dashboard basic styling also npm i @tremor/react and config tailwind with number 8 doc of tremor.so

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -1,9 +1,16 @@
 import { analytics } from "@/utils/analytics";
+import AnalyticsDashboard from "../components/AnalyticsDashboard";
 
 const Page = async () => {
-  const pageview = await analytics.retrieve("pageview", "");
+  const pageview = await analytics.retrieveDays("pageview", 2);
 
-  return <pre>{JSON.stringify(pageview)}</pre>;
+  return (
+    <div className="min-h-screen w-full py-12 flex justify-center items-center">
+      <div className="relative w-full max-w-6xl mx-auto text-white">
+        <AnalyticsDashboard />
+      </div>
+    </div>
+  );
 };
 
 export default Page;
---
+++ src/app/components/AnalyticsDashboard.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AnalyticsDashboard = () => {
+  return (
+    <div className="flex flex-col gap-6">
+      <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6"></div>
+    </div>
+  );
+};
+
+export default AnalyticsDashboard;
