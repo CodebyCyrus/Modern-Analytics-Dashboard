246dab716f83f029e2dc94db2cd6950b16c9ab1b
PersianDavinci <persiandavinci@gmail.com>
2024-02-15 13:46:30 +0330

    add redis and confige util/analytics.ts and now contected to db and tested

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,3 +1,5 @@
+import { redis } from "@/lib/redis";
+
 type AnalyticsArgs = {
   retention?: number;
 };
@@ -11,6 +13,11 @@ export class Analytics {
   // namespace can be any page like contact me or anything
   async track(namespace: string, event: object = {}) {
     // database call to persist the events
+
+    const key = `analytics::${namespace}`;
+
+    await redis.hincrby(key, JSON.stringify(event), 1);
+    // hash increment by
   }
 }
 
