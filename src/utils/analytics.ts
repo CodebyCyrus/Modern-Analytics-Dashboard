6c2e01d896807380a7a33ec067f6d0623507f2fb
PersianDavinci <persiandavinci@gmail.com>
2024-02-15 14:04:46 +0330

    install date-fns and utils/index for getDate and add getdate to analytics utils

--- src/utils/analytics.ts
+++ src/utils/analytics.ts
@@ -1,7 +1,12 @@
 import { redis } from "@/lib/redis";
+import { getDate } from "@/utils";
 
 type AnalyticsArgs = {
   retention?: number;
+};
+
+type TrackOptions = {
+  persist?: boolean;
 };
 
 export class Analytics {
@@ -11,14 +16,17 @@ export class Analytics {
     if (opts?.retention) this.retention = opts.retention;
   }
   // namespace can be any page like contact me or anything
-  async track(namespace: string, event: object = {}) {
+  async track(namespace: string, event: object = {}, opts?: TrackOptions) {
     // database call to persist the events
 
-    const key = `analytics::${namespace}`;
+    let key = `analytics::${namespace}`;
+    if (!opts?.persist) {
+      key += `::${getDate()}`;
+    }
 
     await redis.hincrby(key, JSON.stringify(event), 1);
     // hash increment by
   }
 }
 
-export const analytics = new Analytics({ retention: 3600 });
+export const analytics = new Analytics();
