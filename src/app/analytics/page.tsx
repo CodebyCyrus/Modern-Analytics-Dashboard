a658c2bb7af5f1e8d82a1aaacdab9ffe0e8fdf19
PersianDavinci <persiandavinci@gmail.com>
2024-02-15 16:06:51 +0330

    add card components to AnalyticsDashboard styling with termor and tailwind the analytics dashboard page and add props and etc for avgVisitorsPerDay and test working fine

--- src/app/analytics/page.tsx
+++ src/app/analytics/page.tsx
@@ -2,12 +2,25 @@ import { analytics } from "@/utils/analytics";
 import AnalyticsDashboard from "../components/AnalyticsDashboard";
 
 const Page = async () => {
-  const pageview = await analytics.retrieveDays("pageview", 2);
+  const TRACKING_DAYS = 7;
+
+  const pageviews = await analytics.retrieveDays("pageview", 7);
+
+  const totalPageviews = pageviews.reduce((acc, curr) => {
+    return (
+      acc +
+      curr.events.reduce((acc, curr) => {
+        return acc + Object.values(curr)[0]!;
+      }, 0)
+    );
+  }, 0);
+
+  const avgVisitorsPerDay = (totalPageviews / TRACKING_DAYS).toFixed(1);
 
   return (
     <div className="min-h-screen w-full py-12 flex justify-center items-center">
       <div className="relative w-full max-w-6xl mx-auto text-white">
-        <AnalyticsDashboard />
+        <AnalyticsDashboard avgVisitorsPerDay={avgVisitorsPerDay} />
       </div>
     </div>
   );
--- src/app/components/AnalyticsDashboard.tsx
+++ src/app/components/AnalyticsDashboard.tsx
@@ -1,9 +1,25 @@
+"use client";
+
+import { Card } from "@tremor/react";
 import React from "react";
 
-const AnalyticsDashboard = () => {
+interface AnalyticsDashboardProps {
+  avgVisitorsPerDay: string;
+}
+
+const AnalyticsDashboard = ({ avgVisitorsPerDay }: AnalyticsDashboardProps) => {
   return (
     <div className="flex flex-col gap-6">
-      <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6"></div>
+      <div className="grid w-full mx-auto grid-cols-1 sm:grid-cols-2 gap-6">
+        <Card className="w-full mx-auto max-w-xs">
+          <p className="text-tremor-default text-dark-tremor-content">
+            Avg. visitors/day
+          </p>
+          <p className="text-3xl text-dark-tremor-content-strong font-semibold">
+            {avgVisitorsPerDay}
+          </p>
+        </Card>
+      </div>
     </div>
   );
 };
